fa366c16d59486b707726ee946c65437334c6c22
LomyPC\Lomy <lrgmincher@gmail.com>
2018-11-08 12:45:10 +0000

    Added mappage

--- ActivityWebsite/src/app/app-routing.module.ts
+++ ActivityWebsite/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MappageComponent } from './mappage/mappage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'app-mappage', component: MappageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
